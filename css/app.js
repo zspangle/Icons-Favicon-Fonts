b42d00ae44a301ae3343c763a4e29872bbdf8b8b
zspangle <79054674+zspangle@users.noreply.github.com>
2021-04-18 19:50:44 -0400

    First Commit

---
+++ css/app.js
@@ -0,0 +1,13 @@
+
+$(".dropDownMenu").click(function(){
+    $(".navWrapper").toggleClass("toggle");
+});
+
+$(window).resize(function() {
+    if (window.innerWidth >= 600 && $(".navWrapper").hasClass("toggle")){
+        $(".navWrapper").removeClass("toggle")
+    }else if(window.innerWidth<600&&clicked){
+        $(".navWrapper").addClass("toggle");
+    }
+});
+
